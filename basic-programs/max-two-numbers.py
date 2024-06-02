ef8058b6b2066385e57588a5b35795ea15848d5e
shrutigajul <gajul.shruti20@gmail.com>
2024-06-02 12:16:36 -0400

    max_of_two_numbers

---
+++ basic-programs/max-two-numbers.py
@@ -0,0 +1,16 @@
+# Given two numbers, write a Python code to find the Maximum of these two numbers.
+# Python program to find the
+# maximum of two numbers
+
+
+def maximum(a, b):
+	
+	if a >= b:
+		return a
+	else:
+		return b
+	
+# Driver code
+a = 2
+b = 4
+print(maximum(a, b))
