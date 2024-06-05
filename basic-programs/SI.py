9d1a8726632eb43cada39d39e6b81216201bcacf
shrutigajul <gajul.shruti20@gmail.com>
2024-06-05 15:21:34 -0400

    Simple-interest-type2

--- basic-programs/SI.py
+++ basic-programs/SI.py
@@ -1,5 +1,5 @@
 # Python Program for Simple Interest
-
+# Type 1(ASKING USER TO INPUT CODE)
 p=int(input('what is the principal ?'))
 t=int (input('what is the time period ?'))
 r=int(input('what is the rate of interest?'))
@@ -10,3 +10,16 @@ def SI(p,t,r):
 print('the time is ',t)
 print('the rate of interest is ',r)
 print('the simple interest is ',Simple_interest)
+
+# Type2(GIVING THE VALUES BEFORE RUNNING THE CODE)
+
+def simpleinterest(p,t,r):
+    print('the principal amt is',p)
+    print('the time period is',t)
+    print('the rate of interest is',r)
+
+    si=p*t*r/100
+
+    print(simpleinterest, si)
+    return si
+simpleinterest(3000,4,2)
