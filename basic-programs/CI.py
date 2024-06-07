e8b606f9b68681091080052976455994fbf7e610
shrutigajul <gajul.shruti20@gmail.com>
2024-06-06 23:08:12 -0400

    Compound_interest_Sums

--- basic-programs/CI.py
+++ basic-programs/CI.py
@@ -8,7 +8,37 @@ def compond_interest(P,R,T): #doubt when defined amt(p,t,r) ci is not defined wh
     Rate=int(input('what is the rate of interest ?',R))
     Time=int(input('what is the time period ?', T ))
 
-    Amount=Principal*(pow(1+Rate/100,Time))
+    Amount=Principal*pow((1+Rate/100,Time))
     compond_interest=Amount-Principal
 
-print('the coumpond interest is ',compond_interest)
+    print('the coumpond interest is ',compond_interest)
+
+# Type2 :giving input
+
+# def CI(p,r,t):
+#     print('the principal is ',p )
+#     print('the rate of interest is ',r)
+#     print('the time period is ',t)
+    
+#     amount=p* pow((1+r/100),t)
+#     CI = amount-p
+#     print('compund interest is', CI)
+#     return CI
+
+# CI(4000,3,2)
+
+# Type3 :writing code with pow function
+# ** =expontional
+
+# def CI(p,r,t):
+#     print('the principal is ',p )
+#     print('the rate of interest is ',r)
+#     print('the time period is ',t)
+    
+#     amount=(p *(1+r/100)**t)
+#     CI = amount-p
+#     print('compund interest is', CI)
+#     return CI
+
+# CI(4000,2,3)
+
